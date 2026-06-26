9dd82f4157e9eba753516676a9d40d89e1596ae6
jmp1812 <jmp1812@rit.edu>
2026-06-25 20:13:15 -0400

    Established Properties file for SQL switching, completed connection to database class, and started adding DDL Parsing elements to initializer.

--- src/main/java/com/db_connector/template/interaction/DatabaseConnection.java
+++ src/main/java/com/db_connector/template/interaction/DatabaseConnection.java
@@ -1,19 +1,55 @@
 package com.db_connector.template.interaction;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.IOException;
 
 public class DatabaseConnection {
-    private static final String CONNECTION_PATH = "jdbc:sqlite:database/template.db";
+    private static final Properties properties = new Properties();
 
-    public Connection getConnection() throws SQLException{
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("SQLite JDBC Driver missing from classpath.", e);
+    /**
+     * Used to load the properties from the config.properties file upon being loaded into memory
+     */
+    static {
+        try(InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("config.properties")){
+            if (input == null) {
+                throw new RuntimeException("Unable to find config.properties");
+            }
+            properties.load(input);
+        } catch (IOException e){
+            throw new RuntimeException("ERROR: Couldn't load configuration file.");
         }
+    }
 
-        return DriverManager.getConnection(CONNECTION_PATH);
+    /**
+     * Method used for establishing a connection between the desired database,
+     * using properties from external file to allow for universal code between
+     * specific database types.
+     * @return Connection, to utilize the database
+     * @throws SQLException, if driver is not initalized or connection fails
+     */
+    public Connection getConnection() throws SQLException{
+        /**
+         * Collects information from config.properties file, requires manual switch
+         * for change between SQLite and PostGRES
+         */
+        String driver = properties.getProperty("db.driver");
+        String url = properties.getProperty("db.url");
+        
+        // using Connection Props for better security
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", properties.getProperty("db.user", ""));
+        connectionProps.put("password", properties.getProperty("db.password", ""));
+        
+        // try catch for the driver, where if it cannot find the related driver, will crash.
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Database Driver missing: " + driver, e);
+        }
+        return DriverManager.getConnection(url, connectionProps);
     }
 }
---
+++ src/main/java/com/db_connector/template/interaction/DatabaseInitializer.java
@@ -0,0 +1,85 @@
+package com.db_connector.template.interaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.lang.StringBuilder;
+
+public class DatabaseInitializer {
+    DatabaseConnection dbConnection;
+
+    public DatabaseInitializer(DatabaseConnection dbConnection){
+        this.dbConnection = dbConnection;
+    }
+
+    public void createTable(String tableName, String[] varNames, String[] types) throws SQLException{
+        // Simple checks to ensure that statement is correctly formatted for the table.
+        if (varNames == null || types == null || varNames.length != types.length){
+            throw new IllegalArgumentException("Statement is not correct format.");
+        }
+
+        // Create the statement from its parts.
+        StringBuilder createString = new StringBuilder();
+
+        // Attaches the correlating tableName variable.
+        // createString will be attached as the following:
+        // createString = "create table if not exists [TABLENAME] (""
+        createString.append("create table if not exists ").append(tableName).append(" (");
+        
+        // now adding all the varNames and types attached to it, including all the constraints
+        for (int i = 0; i < varNames.length; i++){
+            // formatted like:
+            // "fruit VARCHAR(25) PRIMARY KEY"
+            createString.append(varNames[i]).append(" ").append(types[i]);
+            
+            // simple check to ensure that the last pairing doesn't insert a comma
+            if (i < varNames.length - 1){
+                createString.append(", ");
+            }
+        }
+
+        // additionally add the closing ");"
+        createString.append(");");
+
+        // try, catch block for both statements as both can result in SQLException errors.
+        try (statementExecutor(createString)) {
+            System.out.println("Table '" + tableName + "' verified/created successfully.");
+        } catch (SQLException e) {
+            throw new SQLException("Table could not be created for " + tableName, e);
+        }
+    }
+
+    public void dropTable(String tableName, boolean cascade) throws SQLException{
+        // Create the statement from its parts.
+        StringBuilder createString = new StringBuilder();
+        createString.append("drop table if exists ").append(tableName);
+        if (cascade){
+            createString.append(" cascade");
+        }
+        createString.append(";");
+
+        // try, catch block for both statements as both can result in SQLException errors.
+        try (Connection database = dbConnection.getConnection(); Statement stmt = database.createStatement()) {
+            // attempt to execute this string to the database file.
+            stmt.executeUpdate(createString.toString());
+            System.out.println("Table '" + tableName + "' verified/created successfully.");
+        } catch (SQLException e) {
+            throw new SQLException("Table could not be created for " + tableName, e);
+        }
+    }
+
+    public void alterTable(String tableName, String alterType, String[] items){
+
+    }
+
+    private boolean statementExecutor(StringBuilder createString) throws SQLException{
+        // try, catch block for both statements as both can result in SQLException errors.
+        try (Connection database = dbConnection.getConnection(); Statement stmt = database.createStatement()) {
+            // attempt to execute this string to the database file.
+            stmt.executeUpdate(createString.toString());
+            return true;
+        } catch (SQLException e) { 
+            throw new SQLException(e);
+        }
+    }
+}
